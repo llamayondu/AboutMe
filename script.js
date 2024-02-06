a82dcdf8354bb00c5d1832adc3376eb43f045551
Shreeprabhas <shreeprabhas@gmail.com>
2024-02-06 18:32:10 +0530

    responsive and blogs and content

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function showOverlay(section) {
             document.getElementById('overlay-text').innerHTML = 'I am smart.<br>I can play the drums.<br>I am good at Valorant.';
             break;
         case 'About':
-            document.getElementById('overlay-text').innerHTML = 'I am Prabhas Varma. I like math and Valorant. <br>I have lived in Hyderabad all my life and hence I also love Biryani.<br><br> I am approximately 5 feet and 10 inches tall. <br>I like food and games a lot. ';
+            document.getElementById('overlay-text').innerHTML = 'I am Prabhas Varma. I like math and Valorant. <br>I have lived in Hyderabad all my life and hence I also love Biryani.<br><br> I am approximately 5 feet and 10 inches tall. <br>I like food and games a lot.<br>Also, I like ducks ';
             break;
     }
     document.getElementById('overlay').style.display = 'flex';
@@ -51,6 +51,7 @@ document.addEventListener("DOMContentLoaded", function (){
     var icon = document.getElementById("icon");
     var github_icon = document.getElementById("github-icon"); 
     var insta_icon = document.getElementById("insta-icon"); 
+    var mail_icon = document.getElementById("mail-icon");
 
     console.log(icon);
 
@@ -60,11 +61,13 @@ document.addEventListener("DOMContentLoaded", function (){
             icon.src="moon.png";
             github_icon.src="github-black.png";
             insta_icon.src="insta-black.png";
+            mail_icon.src="mail-black.png";
 
         } else {
             icon.src="sun.png";
             github_icon.src="github-white.png";
             insta_icon.src="insta-white.png";
+            mail_icon.src="mail-white.png";
         }
     }
 
